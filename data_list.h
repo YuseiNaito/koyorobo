86a423e2b37522f6aef273757bbd22fa7d274703
yuseinaito <yusei.vitz.787@outlook.jp>
2018-09-02 03:36:35 +0900

    first

--- data_list.h
+++ data_list.h
@@ -2,19 +2,21 @@
 #define KOYO_DATA_LIST_H
 
 namespace koyo {
-namespace data_list {
-// for input
-struct signal {
-  // sample
-  int test_data;
-};
+	namespace data_list {
+		// for input
+		struct signal {
+			double vartical;   // c•ûŒü
+			double horizontal; // ‰ñ“]•ûŒü
+			bool button0;      // ƒ{ƒ^ƒ“
+		};
 
-// for output
-struct command {
-  // sample
-  int test_data;
-};
-}
+		// for output
+		struct command {
+			double vel;    // ‘¬“x
+			double omega;  // Šp‘¬“x
+			double weapon; // UŒ‚
+		};
+	}
 }
 
 #endif
