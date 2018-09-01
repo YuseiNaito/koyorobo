e757a6b417d19d0989295fd94cd45ca6f0eeb13f
yuseinaito <yusei.vitz.787@outlook.jp>
2018-09-02 03:35:53 +0900

    first

--- controller.cpp
+++ controller.cpp
@@ -7,11 +7,47 @@ namespace controller {
 controller::controller() {}
 
 data_list::command controller::execute() {
-  // sample
-  command_.test_data *= 2;
+  // PWM出力が可能なポートにつながっている場合
+  dt1 = (command_.omega > 0) ? dt_max * command_.vel / vel_max
+                             : dt_max * command_.vel / vel_max *
+                                   (1 + command_.omega / omega_max);
+  dt2 = (command_.omega < 0) ? dt_max * command_.vel / vel_max
+                             : dt_max * command_.vel / vel_max *
+                                   (1 - command_.omega / omega_max);
+  // motor_left
+  if (dt1 > 0) {
+    analogWrite(pwmpin_L1, 0);
+    // delay(50); // デッドタイムがいるならディレイ
+    analogWrite(pwmpin_L2, (int)(pwm_max * dt1));
+  } else if (dt1 < 0) {
+    analogWrite(pwmpin_L2, 0);
+    // delay(50);
+    analogWrite(pwmpin_L1, (int)(pwm_max * (-dt1)));
+  } else if (dt1 == 0) {
+    analogWrite(pwmpin_L1, 0);
+    analogWrite(pwmpin_L2, 0);
+  }
 
-  // sample
-  Serial.println(command_.test_data);
+  // motor_right
+  if (dt2 > 0) {
+    analogWrite(pwmpin_R1, 0);
+    // delay(50);
+    analogWrite(pwmpin_R2, (int)(pwm_max * dt2));
+  } else if (dt2 < 0) {
+    analogWrite(pwmpin_R2, 0);
+    // delay(50);
+    analogWrite(pwmpin_R1, (int)(pwm_max * (-dt2)));
+  } else if (dt2 == 0) {
+    analogWrite(pwmpin_R1, 0);
+    analogWrite(pwmpin_R2, 0);
+  }
+
+  // motor_kick
+  if (command_.weapon > 0) {
+    analogWrite(pwmpin_kick, (int)(pwm_max * command_.weapon / weapon_max));
+  } else {
+    analogWrite(pwmpin_kick, 0);
+  }
 
   return command_;
 }
--- controller.h
+++ controller.h
@@ -1,5 +1,6 @@
 #ifndef KOYO_CONTROLLER_H
 #define KOYO_CONTROLLER_H
+#include "arduino.h"
 #include "data_list.h"
 
 namespace koyo {
@@ -12,6 +13,21 @@ class controller {
 
  private:
   data_list::command command_;
+  // test
+  double dt1 = 0;
+  double dt2 = 0;
+
+  const double dt_max = 1;
+  const double vel_max = (0xFF) * 100;
+  const double omega_max = (0xFF) * 3.14;
+  const double weapon_max = 120 * 2;
+
+  const int pwm_max = (0xFF);
+  const int pwmpin_L1 = 5;
+  const int pwmpin_L2 = 6;
+  const int pwmpin_R1 = 9;
+  const int pwmpin_R2 = 10;
+  const int pwmpin_kick = 11;
 };
 }  // namespace controller
 }  // namespace koyo
