942ee1b49b25de5c14f2b1768774266c58a90d30
yuseinaito <yusei.vitz.787@outlook.jp>
2018-09-02 22:41:27 +0900

    modified action to use data_list's parameter

--- action_normal.cpp
+++ action_normal.cpp
@@ -3,9 +3,14 @@
 namespace koyo {
 namespace action {
 data_list::command normal::execute() {
-  command_.vel = signal_.vartical * 100;
-  command_.omega = signal_.horizontal * 3.14;
-  command_.weapon = 120 * signal_.button0;
+  // ç≈ëÂë¨ìxÇÃ70%
+  constexpr double vel_coef =
+      0.7 * data_list::vel_max / data_list::stick_res_max;
+  constexpr double omega_coef = data_list::omega_max / data_list::stick_res_max;
+
+  command_.vel = signal_.vartical * vel_coef;
+  command_.omega = signal_.horizontal * omega_coef;
+  command_.weapon = signal_.button0 * data_list::weapon_max;
   return command_;
 }
 }  // namespace action
