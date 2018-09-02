365ed8cc81f2ccb163f7ee6e1d1c169d56303112
yuseinaito <yusei.vitz.787@outlook.jp>
2018-09-02 22:34:17 +0900

    add vel_max, omega_max, weapon_max, input param

--- data_list.h
+++ data_list.h
@@ -1,22 +1,35 @@
 #ifndef KOYO_DATA_LIST_H
 #define KOYO_DATA_LIST_H
 
-namespace koyo {
-	namespace data_list {
-		// for input
-		struct signal {
-			double vartical;   // c•ûŒü
-			double horizontal; // ‰ñ“]•ûŒü
-			bool button0;      // ƒ{ƒ^ƒ“
-		};
+#define _USE_MATH_DEFINES
 
-		// for output
-		struct command {
-			double vel;    // ‘¬“x
-			double omega;  // Šp‘¬“x
-			double weapon; // UŒ‚
-		};
-	}
-}
+#include "F710.h"
+
+#include <math.h>
+
+namespace koyo {
+namespace data_list {
+// for input
+struct signal {
+  double vartical;    // c•ûŒü
+  double horizontal;  // ‰ñ“]•ûŒü
+  bool button0;       // ƒ{ƒ^ƒ“
+};
+
+// for output
+struct command {
+  double vel;     // ‘¬“x
+  double omega;   // Šp‘¬“x
+  double weapon;  // UŒ‚
+};
+
+constexpr double wheel_radius{25};
+
+constexpr double vel_max{100 * 2 * M_PI * wheel_radius};
+constexpr double omega_max{M_PI};
+constexpr double weapon_max{100};
+constexpr double stick_res_max{F710::stick_res_max};
+}  // namespace data_list
+}  // namespace koyo
 
 #endif
