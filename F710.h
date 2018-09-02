a7c69f93d862665ff42da81d31ad060543cdb38d
yuseinaito <yusei.vitz.787@outlook.jp>
2018-09-02 14:56:10 +0900

    add F710's param file

---
+++ F710.h
@@ -0,0 +1,11 @@
+#ifndef KOYO_F710_H
+#define KOYO_F710_H
+
+namespace koyo {
+namespace F710 {
+// -255 ~ +255
+constexpr double stick_res_max{255};
+}  // namespace F710
+}  // namespace koyo
+
+#endif
