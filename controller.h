34f6c722140eec1c92a5c4d8530ccda0b7920de4
yuseinaito <yusei.vitz.787@outlook.jp>
2018-10-01 11:41:11 +0900

    edit controller for circuit

--- controller.h
+++ controller.h
@@ -14,17 +14,24 @@ class controller {
  private:
   data_list::command command_;
   // test
-  double dt1_;
-  double dt2_;
+  double dt_L_;
+  double dt_R_;
+  double dt_kick_;
 
   const double dt_max_;
-
   const int pwm_max_;
-  const int pwmpin_L1_;
-  const int pwmpin_L2_;
-  const int pwmpin_R1_;
-  const int pwmpin_R2_;
-  const int pwmpin_kick_;
+  
+  const int pwm_pin_L_;
+  const int pwm_pin_R_;
+  const int pwm_pin_kick_;
+
+  bool motor_pin_L1_;
+  bool motor_pin_L2_;
+  bool motor_pin_R1_;
+  bool motor_pin_R2_;
+  bool motor_pin_kick1_;
+  bool motor_pin_kick2_;
+  
 };
 }  // namespace controller
 }  // namespace koyo
