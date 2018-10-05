34221ef33b9f1feaeb8397268b53c653b12957a9
yuseinaito <yusei.vitz.787@outlook.jp>
2018-10-05 23:04:56 +0900

    add main sketch

--- koyorobo.ino
+++ koyorobo.ino
@@ -1,3 +1,33 @@
-void setup() {}
+#include "action_base.h"
+#include "action_normal.h"
+#include "controller.h"
+#include "data_list.h"
+#include "receiver.h"
+#include "state_switcher.h"
 
-void loop() {}
+bool run_flag;
+
+koyo::controller::controller controller;
+koyo::receiver::receiver receiver;
+koyo::data_list::signal signal = receiver.execute();
+koyo::state_switcher::state_switcher switcher(signal);
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  signal = receiver.execute();
+
+  /*
+  Serial.print(" vartical=");
+  Serial.print(signal.vartical);
+  Serial.print(" horizontal=");
+  Serial.print(signal.horizontal);
+  Serial.print(" button0=");
+  Serial.println(signal.button0);*/
+
+  controller.set_command(switcher.execute()->execute());
+  controller.execute();
+  delay(50); // 遅延時間は実機で調整
+}
