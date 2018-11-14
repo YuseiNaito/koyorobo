6536eb739330ff56cb1e89058b31ad2c2929b8dc
Naito Yusei <yusei.vitz.787@gmail.com>
2018-11-14 23:45:31 +0900

    add motor parameters file

---
+++ motors_parameter.h
@@ -0,0 +1,10 @@
+#ifndef KOYO_MOTORS_PARAMETER_H
+#define KOYO_MOTORS_PARAMETER_H
+
+namespace koyo {
+namespace motors_parameter {
+
+}  // namespace motors_parameter
+}  // namespace koyo
+
+#endif
