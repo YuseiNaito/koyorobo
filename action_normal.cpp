c4de19ea8ca54bc14cf9b2ef05268bcae349e02c
Naito Yusei <yusei.vitz.787@gmail.com>
2018-11-14 23:44:54 +0900

    edit for koyofes2018

--- action_normal.cpp
+++ action_normal.cpp
@@ -3,14 +3,14 @@
 namespace koyo {
 namespace action {
 data_list::command normal::execute() {
-  // Å‘å‘¬“x‚Ì70%
+  // ï¿½Å‘å‘¬ï¿½xï¿½ï¿½70%
   constexpr double vel_coef =
-      0.7 * data_list::vel_max / data_list::stick_res_max;
+    0.7 * data_list::vel_max / data_list::stick_res_max;
   constexpr double omega_coef = data_list::omega_max / data_list::stick_res_max;
 
   command_.vel = signal_.vartical * vel_coef;
   command_.omega = signal_.horizontal * omega_coef;
-  command_.weapon = signal_.button0 * data_list::weapon_max;
+  command_.weapon = 0.6 * signal_.button0 * data_list::weapon_max;
   return command_;
 }
 }  // namespace action
--- controller.cpp
+++ controller.cpp
@@ -11,14 +11,32 @@ controller::controller()
     dt_max_(1),
     pwm_max_((0xFF)),
     pwm_pin_L_(3),
-    pwm_pin_R_(11),//before 10
-    pwm_pin_kick_(10),//before 5
-    motor_pin_L1_(2),
-    motor_pin_L2_(4),
-    motor_pin_R1_(7),
-    motor_pin_R2_(8),
-    motor_pin_kick1_(12),
-    motor_pin_kick2_(13)
+      pwm_pin_R_(9),
+      pwm_pin_kick_(11),
+      motor_pin_L1_(2),
+      motor_pin_L2_(4),
+      motor_pin_R1_(8),
+      motor_pin_R2_(10),
+      motor_pin_kick1_(13),
+      motor_pin_kick2_(5)
+    /*
+      //for A's driver
+      pwm_pin_L_(0),
+      pwm_pin_R_(1),
+      pwm_pin_kick_(2)
+    */
+    /*
+      //for B's hyper
+      pwm_pin_L_(3),
+      pwm_pin_R_(9),
+      pwm_pin_kick_(10),
+      motor_pin_L1_(2),
+      motor_pin_L2_(4),
+      motor_pin_R1_(8),
+      motor_pin_R2_(7),
+      motor_pin_kick1_(6),
+      motor_pin_kick2_(5)
+    */
 {
 
   pinMode(pwm_pin_L_, OUTPUT);
@@ -36,6 +54,11 @@ controller::controller()
   bias_.vel = 0.05;
   bias_.omega = 0.05;
   bias_.weapon = 0.05;
+  /*//for B's hyper
+    digitalWrite(motor_pin_L2_, HIGH);
+    digitalWrite(motor_pin_R2_, HIGH);
+    digitalWrite(motor_pin_kick2_, HIGH);
+  */
 }
 
 data_list::command controller::execute() {
@@ -44,17 +67,18 @@ data_list::command controller::execute() {
   before_.omega += bias_.omega * (command_.omega - before_.omega);
   before_.weapon += bias_.weapon * (command_.weapon - before_.weapon);
 
-  if (command_.vel == 0.0 && fabs(before_.vel) < (data_list::vel_max * 0.05) )before_.vel = 0.0; //5%以下カット
-  if (command_.omega == 0.0 && fabs(before_.omega) < (data_list::omega_max * 0.05) )before_.omega = 0.0; //5%以下カット
-  if (command_.weapon == 0.0 && fabs(before_.weapon) < (data_list::weapon_max * 0.05) )before_.weapon = 0.0; //5%以下カット
+  if (command_.vel == 0.0 && fabs(before_.vel) < (data_list::vel_max * 0.1) )before_.vel = 0.0; //10%以下カット
+  if (command_.omega == 0.0 && fabs(before_.omega) < (data_list::omega_max * 0.1) )before_.omega = 0.0; //10%以下カット
+  if (command_.weapon == 0.0 && fabs(before_.weapon) < (data_list::weapon_max * 0.1) )before_.weapon = 0.0; //10%以下カット
 
-  dt_L_ = (before_.omega > 0) ? dt_max_ * before_.vel / data_list::vel_max
+  dt_R_ = (before_.omega > 0) ? dt_max_ * before_.vel / data_list::vel_max
           : dt_max_ * before_.vel / data_list::vel_max *
           (1 + before_.omega / data_list::omega_max);//test
-  dt_R_ = (before_.omega < 0) ? dt_max_ * before_.vel / data_list::vel_max
+  dt_L_ = (before_.omega < 0) ? dt_max_ * before_.vel / data_list::vel_max
           : dt_max_ * before_.vel / data_list::vel_max *
           (1 - before_.omega / data_list::omega_max);//test
   dt_kick_ = before_.weapon / data_list::weapon_max;
+
 
   // motor_left
   if (dt_L_ > 0) {
@@ -70,9 +94,9 @@ data_list::command controller::execute() {
 
     analogWrite(pwm_pin_L_, (int)(pwm_max_ * (-dt_L_)));
   } else {
-    digitalWrite(motor_pin_L1_, HIGH);
+    digitalWrite(motor_pin_L1_, LOW);
     //delay?
-    digitalWrite(motor_pin_L2_, HIGH);
+    digitalWrite(motor_pin_L2_, LOW);
 
     digitalWrite(pwm_pin_L_, LOW);
   }
@@ -91,9 +115,9 @@ data_list::command controller::execute() {
 
     analogWrite(pwm_pin_R_, (int)(pwm_max_ * (-dt_R_)));
   } else {
-    digitalWrite(motor_pin_R1_, HIGH);
+    digitalWrite(motor_pin_R1_, LOW);
     //delay?
-    digitalWrite(motor_pin_R2_, HIGH);
+    digitalWrite(motor_pin_R2_, LOW);
 
     digitalWrite(pwm_pin_R_, LOW);
   }
@@ -106,13 +130,73 @@ data_list::command controller::execute() {
 
     analogWrite(pwm_pin_kick_, (int)(pwm_max_ * dt_kick_));
   } else {
-    digitalWrite(motor_pin_kick1_, HIGH);
+    digitalWrite(motor_pin_kick1_, LOW);
     //delay?
-    digitalWrite(motor_pin_kick2_, HIGH);
+    digitalWrite(motor_pin_kick2_, LOW);
 
     digitalWrite(pwm_pin_kick_, LOW);
   }
+  /*
+    //for A's driver
+    char buf[20]={"\0"};
+    sprintf(buf, "sel %d", pwm_pin_L_ + 16);
+    Serial.println(buf);
+    Serial.flush();
+    sprintf(buf, "dt %d", (int)(100 * dt_L_));
+    Serial.println(buf);
+    Serial.flush();
 
+    sprintf(buf, "sel %d", pwm_pin_R_ + 16);
+    Serial.println(buf);
+    Serial.flush();
+    sprintf(buf, "dt %d", (int)(100 * dt_R_));
+    Serial.println(buf);
+    Serial.flush();
+
+
+    sprintf(buf, "sel %d", pwm_pin_kick_ + 16);
+    Serial.println(buf);
+    Serial.flush();
+    sprintf(buf, "dt %d", (int)(100 * dt_kick_));
+    Serial.println(buf);
+    Serial.flush();
+  */
+
+
+  /* //for B's hyper
+    // motor_left
+    if (dt_L_ > 0) {
+      digitalWrite(motor_pin_L1_, LOW);
+      analogWrite(pwm_pin_L_, (int)(pwm_max_ * dt_L_));
+    } else if (dt_L_ < 0) {
+      digitalWrite(motor_pin_L1_, HIGH);
+      analogWrite(pwm_pin_L_, (int)(pwm_max_ * (-dt_L_)));
+    } else {
+      digitalWrite(motor_pin_L1_, LOW);
+      digitalWrite(pwm_pin_L_, LOW);
+    }
+
+    // motor_right
+    if (dt_R_ > 0) {
+      digitalWrite(motor_pin_R1_, HIGH);
+      analogWrite(pwm_pin_R_, (int)(pwm_max_ * dt_R_));
+    } else if (dt_R_ < 0) {
+      digitalWrite(motor_pin_R1_, LOW);
+      analogWrite(pwm_pin_R_, (int)(pwm_max_ * (-dt_R_)));
+    } else {
+      digitalWrite(motor_pin_R1_, LOW);
+      digitalWrite(pwm_pin_R_, LOW);
+    }
+
+    // motor_kick
+    if (dt_kick_ > 0) {
+      digitalWrite(motor_pin_kick1_, LOW);
+      analogWrite(pwm_pin_kick_, (int)(pwm_max_ * dt_kick_));
+    } else {
+      digitalWrite(motor_pin_kick1_, LOW);
+      digitalWrite(pwm_pin_kick_, LOW);
+    }
+  */
   return command_;
 }
 
